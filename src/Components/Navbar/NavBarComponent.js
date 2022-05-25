e13d3d8efe99b723f073da806bb4b6a671c0a80a
carolina <carito.92@gmail.com>
2022-05-25 19:29:44 -0400

    se añade un navbar con más botones

--- src/Components/Navbar/NavBarComponent.js
+++ src/Components/Navbar/NavBarComponent.js
@@ -1,6 +1,6 @@
 import {
-    Container, /*Row, Col, Form, Input, Button,*/ Navbar, /*Nav,
-    NavbarBrand, NavLink, NavItem, UncontrolledDropdown,
+    Container, /*Row, Col, Form, Input, Button,*/ Navbar, Nav,
+   /* NavbarBrand, NavLink, NavItem, UncontrolledDropdown,
     DropdownToggle, DropdownMenu, DropdownItem*/
 } from 'react-bootstrap';
 export default NavBarComponent
@@ -8,13 +8,15 @@ export default NavBarComponent
 function NavBarComponent() {
     return <>
 
-        <Container>
-            <Navbar expand="lg" variant="light" bg="light">
-                <Container>
-                    <Navbar.Brand href="#">Adela's Garden</Navbar.Brand>
-                </Container>
-            </Navbar>
-        </Container>
-
-    </>
+  <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Adela's Garden</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Macetas</Nav.Link>
+      <Nav.Link href="#features">Plantas</Nav.Link>
+      <Nav.Link href="#pricing">¿Quienes somos?</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+</>
 }
