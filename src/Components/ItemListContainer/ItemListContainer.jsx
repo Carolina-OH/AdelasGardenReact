da5c7c6cd623496044442ecc4dfe206f5d31550e
carolina <carito.92@gmail.com>
2022-06-08 03:25:23 -0400

    se añade key

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -88,7 +88,7 @@ const ItemListContainer = () => {
   return (
     <section className="Contenedor card-group g-4 m-4 justify-content-center">
       {items.map((item) => 
-        <div>
+        <div key={item.id}>
 
           <Counter stock="10" inicial="1" nombre={item.nombre} des={item.des} img={item.imagen} precio={item.precio}/>
 
